09298fb7119784ca5fcf128eb2648a11d6de2aed
SHapeloglu <154635217+SHapeloglu@users.noreply.github.com>
2026-09-17 10:39:09 +0300

    Show Mailboxes button in portrait split view

--- Classes/IMBAppDelegate.m
+++ Classes/IMBAppDelegate.m
@@ -20,18 +20,51 @@
     titleLabel.textAlignment = UITextAlignmentCenter;
     titleLabel.numberOfLines = 0;
     titleLabel.font = [UIFont boldSystemFontOfSize:24.0f];
-    titleLabel.text = @"iPad1MailBox\n\nAdd an account with the + button.";
+    titleLabel.text = @"iPad1MailBox\n\nOpen Mailboxes and tap + to add an account.";
     [welcome.view addSubview:titleLabel];
 
     UINavigationController *detailNavigation = [[[UINavigationController alloc] initWithRootViewController:welcome] autorelease];
 
     _splitViewController = [[UISplitViewController alloc] init];
+    _splitViewController.delegate = self;
     _splitViewController.viewControllers = [NSArray arrayWithObjects:masterNavigation, detailNavigation, nil];
-    _splitViewController.delegate = nil;
 
     self.window.rootViewController = _splitViewController;
     [self.window makeKeyAndVisible];
     return YES;
+}
+
+- (void)splitViewController:(UISplitViewController *)svc
+     willHideViewController:(UIViewController *)aViewController
+          withBarButtonItem:(UIBarButtonItem *)barButtonItem
+       forPopoverController:(UIPopoverController *)pc {
+    barButtonItem.title = @"Mailboxes";
+
+    if ([svc.viewControllers count] < 2) return;
+    UIViewController *detailContainer = [svc.viewControllers objectAtIndex:1];
+    UIViewController *detail = detailContainer;
+
+    if ([detailContainer isKindOfClass:[UINavigationController class]]) {
+        detail = [(UINavigationController *)detailContainer topViewController];
+    }
+
+    detail.navigationItem.leftBarButtonItem = barButtonItem;
+}
+
+- (void)splitViewController:(UISplitViewController *)svc
+     willShowViewController:(UIViewController *)aViewController
+  invalidatingBarButtonItem:(UIBarButtonItem *)barButtonItem {
+    if ([svc.viewControllers count] < 2) return;
+    UIViewController *detailContainer = [svc.viewControllers objectAtIndex:1];
+    UIViewController *detail = detailContainer;
+
+    if ([detailContainer isKindOfClass:[UINavigationController class]]) {
+        detail = [(UINavigationController *)detailContainer topViewController];
+    }
+
+    if (detail.navigationItem.leftBarButtonItem == barButtonItem) {
+        detail.navigationItem.leftBarButtonItem = nil;
+    }
 }
 
 - (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
@@ -39,6 +72,7 @@
 }
 
 - (void)dealloc {
+    _splitViewController.delegate = nil;
     [_splitViewController release];
     [_window release];
     [super dealloc];
